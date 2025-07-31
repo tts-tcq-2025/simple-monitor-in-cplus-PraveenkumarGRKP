4d12a69371c227a28ecd2b360f090b38baa186bb
PraveenkumarGRKP <grkpraveenkumar@gmail.com>
2025-07-31 20:14:36 +0530

    Update Checker.cpp
    reduced the cyclomatic complexity of the code

--- Checker.cpp
+++ Checker.cpp
@@ -1,22 +1,39 @@
 #include <assert.h>
 #include <iostream>
 using namespace std;
-
-bool batteryIsOk(float temperature, float soc, float chargeRate) {
-  if(temperature < 0 || temperature > 45) {
-    cout << "Temperature out of range!\n";
-    return false;
-  } else if(soc < 20 || soc > 80) {
-    cout << "State of Charge out of range!\n";
-    return false;
-  } else if(chargeRate > 0.8) {
-    cout << "Charge Rate out of range!\n";
-    return false;
-  }
-  return true;
+bool Temperature(float temperature)
+{
+    if(temperature < 0 || temperature > 45) 
+    {
+        cout << "Temperature out of range!\n";
+        return false; 
+    }
+    return true;
 }
-
-int main() {
+bool SOC(float soc)
+{
+    if(soc < 20 || soc > 80)
+    {
+        cout << "State of Charge out of range\n";
+        return false; 
+    }
+    return true;
+}
+bool ChargeRate(float chargeRate)
+{
+    if(chargeRate > 0.8)
+    {
+        cout << "Charge Rate out of range!\n";
+        return false;
+    }
+    return true;
+}
+bool batteryIsOk(float temperature, float soc, float chargeRate)
+{
+    return Temperature(temperature) && SOC(soc) && ChargeRate(chargeRate);
+}
+int main() 
+{
   assert(batteryIsOk(25, 70, 0.7) == true);
   assert(batteryIsOk(50, 85, 0) == false);
 }
